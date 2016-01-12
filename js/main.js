d30e244b39e863674fd31625b32e31e8bfb19562
Tim Whitacre <tim@timw.co>
2016-01-11 22:40:21 -0500

    wrapped up v1

---
+++ js/main.js
@@ -0,0 +1,32 @@
+;(function () {
+
+  var cont = $('#cont');
+  var collection = '';
+
+  // Get File
+  $.getJSON('js/data.json', function (res) {
+
+    res.forEach( function (cat) {
+
+      collection += '<a target="_blank" href="http://www.netflix.com/browse/genre/' + cat.id + '" class="collection-item">';
+      collection += '<span class="filter-title">' + cat.title + '</span></a>';
+
+      cat.subs.forEach( function (sub) {
+        collection += '<a target="_blank" href="http://www.netflix.com/browse/genre/' + sub.id + '" class="collection-item sub-category">';
+        collection += '<span class="filter-title">' + sub.title + '</span><span class="sub-of">' + cat.title + '</span></a>';
+      });
+
+    });
+
+    cont.append(collection);
+
+    // Filter Starter
+    $('#search').instaFilta({
+      targets: '.filter-title',
+      sections: '.collection-item',
+      beginsWith: true
+    });
+
+  });
+
+}());
